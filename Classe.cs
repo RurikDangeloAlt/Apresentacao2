e3b9a607ecfadf0b980e3ba3b1c84ce59178ee98
RurikDangeloAlt <rurikclientalt@gmail.com>
2025-06-05 11:42:37 -0300

    001: Classe.cs

---
+++ Classe.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace Apresentacao
+{
+    public class ClasseGenerica<T>
+    {
+        private T valor;
+
+        public ClasseGenerica(T valor)
+        {
+            this.valor = valor;
+        }
+
+        public void ExibirValor()
+        {
+            Console.WriteLine($"Valor test: {valor}");
+        }
+
+        public T ObterValor()
+        {
+            return valor;
+        }
+    }
+}
